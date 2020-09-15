fa5f81e8e12e45dc184bcba79d528a705c6f0914
ifotn <rich@infrontofthenet.com>
2020-09-15 15:58:32 -0400

    Add project files.

---
+++ COMP2084BeerStore/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2084BeerStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084BeerStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            // use our mock Category model to create and display 10 categories
+            // first, create an object to hold a list of categories
+            var categories = new List<Category>();
+
+            for (var i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category " + i.ToString() });
+            }
+
+            // modify the return View statement so that it now accepts a list of categories to pass to the view for display
+            return View(categories);
+        }
+
+        // /Store/Browse
+        public IActionResult Browse(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
